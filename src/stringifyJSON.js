9ac5bedaaee92ba1f198fdc7b8d712c208fc6b4d
Joshua Newman <jonewman1020@gmail.com>
2014-12-30 12:57:19 -0500

    Made a Mocha and Chai Test Suite for my own test cases for stringifyJSON All test cases solved except: 	1)Undefined Values 	2)Function Values I need to figure out how to skip over these!

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,7 +2,29 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
+ var stringifyJSON = function(o) {
+  if (typeof o === 'string') {
+    return '"' + o + '"';
+  } else if (o === undefined) {
+    return o;
+  } else if (o === null) {
+      return 'null';
+  } else if (typeof o !== 'object') {
+    return o.toString();
+  }
+  var arr = Array.isArray(o);
+  var strObj = arr ? '[' : '{';
+    
+  var keys = Object.keys(o);
+  for(var i=0; i<keys.length; i++) {
+    var currKey = keys[i];
+    var val;
 
-var stringifyJSON = function(obj) {
-  // your code goes here
+    val = stringifyJSON(o[currKey]);
+    strObj = arr ? strObj += val + ',' : strObj += '"' + currKey + '"' + ':' + val +',';
+    if(i===keys.length-1) strObj = strObj.slice(0,strObj.length-1);
+  }
+  return strObj += Array.isArray(o) ? ']' : '}';
 };
+
+
